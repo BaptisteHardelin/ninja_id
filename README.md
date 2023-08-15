f337db10ad24508fa2e0563e701596b6d699cf5d
BaptisteHardelin <baptiste.hardelin@gmail.com>
2023-08-15 23:15:41 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# ninja_id
+
+A ninja id card project  from here  https://youtu.be/c063ddhWafo
+
+s
---
+++ android/app/src/main/java/fr/babaprog/ninja_id/MainActivity.java
@@ -0,0 +1,6 @@
+package fr.babaprog.ninja_id;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
