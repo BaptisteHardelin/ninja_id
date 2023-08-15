54f19c80ad834c06712dd1e477b8a1d30223dacb
BaptisteHardelin <baptiste.hardelin@gmail.com>
2023-08-16 00:21:56 +0200

    Add a appbar

--- lib/main.dart
+++ lib/main.dart
@@ -11,6 +11,10 @@ class NinjaCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Ninja ID Card"),
+      ),
+    );
   }
 }
